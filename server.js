00602175eda74f61366837fb1961a4e8c3b17ce5
KasunDimantha <kasundimantha97@gmail.com>
2024-05-24 20:34:04 +0530

    created nodejs packadge

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require("express")
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!!");
 })
 
-app.listen(5
+app.use("/app/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
